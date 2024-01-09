bf6002c3ba9436f6bfddcaa35c4aa9346de3391d
hellopi2 <jameslego2014@gmail.com>
2024-01-09 00:39:59 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const Player1 = {name: "Excel", agent: "Omen", kills: 0, deaths: 0, assists: 0}
+
+
+
+
+
+
+var http = require('http');
+http.createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`)
+    res.write('Yo!');
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(Player1));
+}).listen(process.env.PORT || 3000);
+
+
