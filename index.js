c382d5bbf2d61383df6e166e68dbf112387f4655
hellopi2 <jameslego2014@gmail.com>
2024-01-09 16:44:53 -0800

    Update index.js || First JSON test

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
-const Player1 = {name: "Excel", agent: "Omen", kills: 0, deaths: 0, assists: 0}
+//Setup
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const prompt = require("prompt-sync")({ sigint: true });
+
+function agentSelPlayer(id, portLink, tag) {
+    this.id = id;
+    this.portLink = portLink;
+    this.tag = tag;
+}
+const jsonArr = [];
+//Connect to Database
+var con = mysql.createConnection({
+    host: "sql5.freesqldatabase.com",
+    user: "sql5675756",
+    password: "UgSUiirjf7",
+    database: "sql5675756"
+});
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    pullAgentSelData();
+});
+
+ //Pull agent Select Data
+    function pullAgentSelData() {
+        const workArr = [];
+        sql1 = "SELECT * FROM agentSelect;"
+        con.query(sql1, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            for (let i = 0; i < 10; i++) {
+                console.log(result[i].Tag);
+                jsonArr.push(new agentSelPlayer(i+1, result[i].Agent, result[i].Tag));
+            }
+            console.log(jsonArr)
+        });
+
+
+
+
+
+    }
 
 
 
 
 
 
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    //res.setHeader('Content-Type', 'application/json');
-   // res.end(JSON.stringify(Player1));
-    res.end();
-}).listen(process.env.PORT || 3000);
 
 
+
+
+//Format Respnse
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//Send HTML
+app.get("/", (req, res) => {
+    res.json(jsonArr);
+
+});
+
+
+
+//Send data
+
+
+app.listen(3000);
